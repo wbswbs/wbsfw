3e63a38a61181b8953282d5f314ca215a2b48d2f
wbswbs <wolfgang@blessen.de>
2023-10-27 09:46:41 +0200

    Order of ENV Variable Names

--- Wbs/Config/ENV.php
+++ Wbs/Config/ENV.php
@@ -10,42 +10,42 @@ namespace wbs\Framework\Config;
  */
 class ENV
 {
-    ### App Environment
-    const APP_ENV='APP_ENV';
-    const APP_SECRET='APP_SECRET';
 
+    const PROJECT_NAME = 'PROJECT_NAME';
+
+    ### Path / Need Trailing Slash
+    const URL_ABSOLUTE = 'URL_ABSOLUTE';
+    const ROOT_PATH    = 'ROOT_PATH';
+
+    ### Email of the Site Admin
+    const SITE_ADMIN = 'SITE_ADMIN';
+
+    ### App Environment
+    const APP_ENV    = 'APP_ENV';
+    const APP_SECRET = 'APP_SECRET';
 
     ### Mögliche ZUstände für App Env
     const APP_ENV_LIVE = 'live';
-    const APP_ENV_DEV = 'dev';
-
-    ### Pfade
-    const URL_ABSOLUTE='URL_ABSOLUTE';
-    const ROOT_PATH='ROOT_PATH';
-
-    ### Email of the Site Admin
-    const SITE_ADMIN='SITE_ADMIN';
+    const APP_ENV_DEV  = 'dev';
 
     ### MySQL Database ###
-    const MYSQL_SERVER='MYSQL_SERVER';
-    const MYSQL_DATABASE='MYSQL_DATABASE';
-    const MYSQL_USER='MYSQL_USER';
-    const MYSQL_PASSWORD='MYSQL_PASSWORD';
-    const MYSQL_SALT='MYSQL_SALT';
+    const MYSQL_SERVER   = 'MYSQL_SERVER';
+    const MYSQL_DATABASE = 'MYSQL_DATABASE';
+    const MYSQL_USER     = 'MYSQL_USER';
+    const MYSQL_PASSWORD = 'MYSQL_PASSWORD';
+    const MYSQL_SALT     = 'MYSQL_SALT';
 
-    const SMTP_HOST='SMTP_HOST';
-    const SMTP_PORT='SMTP_PORT';
-    const SMTP_USER='SMTP_USER';
-    const SMTP_PASS='SMTP_PASS';
-    const SMTP_FROM_EMAIL='SMTP_FROM_EMAIL';
-    const SMTP_FROM_NAME='SMTP_FROM_NAME';
+    const SMTP_HOST       = 'SMTP_HOST';
+    const SMTP_PORT       = 'SMTP_PORT';
+    const SMTP_USER       = 'SMTP_USER';
+    const SMTP_PASS       = 'SMTP_PASS';
+    const SMTP_FROM_EMAIL = 'SMTP_FROM_EMAIL';
+    const SMTP_FROM_NAME  = 'SMTP_FROM_NAME';
 
     ###> Zugang und Konfiguration für die eigene API
-    const API_SERVER_USER='API_AUTH_USER';
-    const API_SERVER_PASSWORD='API_AUTH_PASSWORD';
-    const API_DIR='API_DIR';
-
-    const PROJECT_NAME='PROJECT_NAME';
+    const API_SERVER_USER     = 'API_AUTH_USER';
+    const API_SERVER_PASSWORD = 'API_AUTH_PASSWORD';
+    const API_DIR             = 'API_DIR';
 
     /**
      *  Add this method to your class definition if you want an array of class constants
